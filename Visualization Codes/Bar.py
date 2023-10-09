97d426c10e5e5279623dc37ae2e02fa0a0ae611e
nkBbanerjee100 <115945840+nkBbanerjee100@users.noreply.github.com>
2023-10-09 16:34:53 +0530

    Update Bar.py
    I have added the values as numpy.array method and also changed the colors of the specified co-ordinates and added xlabel and ylabel

--- Visualization Codes/Bar.py
+++ Visualization Codes/Bar.py
@@ -1,9 +1,9 @@
-#code to demonstrate bar graph
 import matplotlib.pyplot as plt
+import numpy as np
 
-x = [5, 2, 9, 4, 7]
-y = [10, 5, 8, 4, 2]
-bar_colors = ['blue', 'green', 'red', 'purple', 'orange']
+x = np.array([5,2,9,4,7])
+y = np.array([10, 5, 8, 4, 2])
+bar_colors = ['pink', 'green', 'yellow', 'purple', 'blue']
 plt.bar(x, y, color=bar_colors)
 
 # Add labels to the axes
@@ -12,5 +12,3 @@ plt.ylabel('Y-axis Label')
 
 plt.title('Bar Plot Example')
 plt.show()
-
-
