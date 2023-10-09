377b3939ee3faec084d1a74f04b777918d158bf7
Abir Kumar Nandi <116013922+Abir-0007@users.noreply.github.com>
2023-10-09 19:52:27 +0530

    Update Axis.py
    I added more axises and allign it to a straight line

--- Visualization Codes/Axis.py
+++ Visualization Codes/Axis.py
@@ -1,8 +1,8 @@
 #Code to plot axis of a graph 
-
+import numpy as np
 from matplotlib import pyplot as plt
 
 
-plt.plot([1,2,3,4],[1,4,9,16],'ro')
-plt.axis([0,6,0,20])
+plt.plot(np.array([1,2,3,4]),'ro')
+plt.axis([0,6,0,20,0,30,0,40])
 plt.show()
