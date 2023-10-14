064996ce2a3f60d6afa7380c04295c183bb7fb9c
Anjishnu Ganguly <113181374+Skythrill256@users.noreply.github.com>
2023-10-14 12:27:22 +0530

    fixed the infinite output bug and made the code more clean

--- PythonCodes/PasswordStrengthChecker.py
+++ PythonCodes/PasswordStrengthChecker.py
@@ -1,75 +1,42 @@
 import string
 import getpass
-def check_password_strength():
-    password = getpass.getpass('Enter the password: ')
+
+def check_password_strength(password):
     strength = 0
-    remarks = ''
-    lower_count = upper_count = num_count = wspace_count = special_count = 0
+    remarks = []
 
-    for char in list(password):
-        if char in string.ascii_lowercase:
-            lower_count += 1
-        elif char in string.ascii_uppercase:
-            upper_count += 1
-        elif char in string.digits:
-            num_count += 1
-        elif char == ' ':
-            wspace_count += 1
-        else:
-            special_count += 1
-
-    if lower_count >= 1:
+    if len(password) < 8:
+        remarks.append("Password is too short. It should be at least 8 characters long.")
+    if any(c in string.ascii_lowercase for c in password):
         strength += 1
-    if upper_count >= 1:
+    if any(c in string.ascii_uppercase for c in password):
         strength += 1
-    if num_count >= 1:
+    if any(c in string.digits for c in password):
         strength += 1
-    if wspace_count >= 1:
-        strength += 1
-    if special_count >= 1:
+    if any(c in string.punctuation for c in password):
         strength += 1
 
     if strength == 1:
-        remarks = 'That\'s a very bad password. Change it as soon as possible.'
+        remarks.append("That's a weak password. Consider using a mix of character types.")
     elif strength == 2:
-        remarks = 'That\'s a weak password. You should consider using a tougher password.'
-    elif strength == 3:
-        remarks = 'Your password is okay, but it can be improved.'
-    elif strength == 4:
-        remarks = 'Your password is hard to guess.But you could make it even more secure.'
-    elif strength == 5:
-        remarks = 'Now that\'s one hell of a strong password!!! Hackers don\'t have a chance guessing that password!'
+        remarks.append("Your password is okay, but it can be improved.")
+    elif strength >= 3:
+        remarks.append("Your password is strong.")
 
-    print('Your password has:-')
-    print(f'{lower_count} lowercase letters')
-    print(f'{upper_count} uppercase letters')
-    print(f'{num_count} digits')
-    print(f'{wspace_count} whitespaces')
-    print(f'{special_count} special characters')
-    print(f'Password Score: {strength / 5}')
-    print(f'Remarks: {remarks}')
+    return remarks
 
+def main():
+    print('Welcome to Password Strength Checker')
+    while True:
+        password = getpass.getpass('Enter the password: ')
+        strength_remarks = check_password_strength(password)
+        for remark in strength_remarks:
+            print(remark)
 
-def check_pwd(another_pw=False):
-    valid = False
-    if another_pw:
-        choice = input('Do you want to check another password\'s strength (y/n) : ')
-    else:
-        choice = input('Do you want to check your password\'s strength (y/n) : ')
-
-    while not valid:
-        if choice.lower() == 'y':
-            return True
-        elif choice.lower() == 'n':
+        choice = input('Do you want to check another password\'s strength (y/n): ')
+        if choice.lower() != 'y':
             print('Exiting...')
-            return False
-        else:
-            print('Invalid input...please try again. \n')
-
+            break
 
 if __name__ == '__main__':
-    print('Welcome to Password Strength Checker')
-    check_pw = check_pwd()
-    while check_pw:
-        check_password_strength()
-        check_pw = check_pwd(True)
+    main()
